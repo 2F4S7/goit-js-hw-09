e2a8b8a1704d0c5cf477f8f7f56dc8039a848b34
2F4S7 <nightsta1kerhf@gmail.com>
2021-04-02 04:36:38 +0300

    npm-run-dev-api

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-import "./css/styles.css";
-
-console.log("Мастерская: деплой билда от Parcel на GitHub Pages");
+import './css/styles.css';
+import validatePassword from './js/validate-pass';
+import apiService from './js/api-service';
+console.log(apiService);
+console.log('Мастерская: деплой билда от Parcel на GitHub Pages');
+console.log(validatePassword);
---
+++ src/js/api-service.js
@@ -0,0 +1,11 @@
+const fetchAllUsers = () => {
+  console.log('fetchAllUsers');
+};
+const fetchUserById = id => {
+  console.log('fetchUserById');
+};
+const updateUserById = id => {
+  console.log('updateUserById');
+};
+
+export default { fetchAllUsers, fetchUserById, updateUserById };
